5ae68488d211f4af0e8bd71636bbee685edc703a
Your Name <you@example.com>
2018-04-20 11:47:25 -0300

    added cross domain configuration

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
 app = require('express')();
+var allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
 
+    next();
+}
+app.use(allowCrossDomain);
 app.get('/', function(req, res){
     //if(<parametros que você deseja testar>)
     res.send('oi');
