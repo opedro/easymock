33dcd86bcd374e7a78b3f2bb94d095d7caa99fc5
Pedro Ruiz <opedro.ruiz@gmail.com>
2018-04-24 20:54:59 -0300

    auto generating from .esm files of 'mocks' folder

--- app.js
+++ app.js
@@ -1,20 +1,65 @@
 app = require('express')();
-var allowCrossDomain = function(req, res, next) {
+fs = require('fs');
+
+var allowCrossDomain = function (req, res, next) {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
     res.header('Access-Control-Allow-Headers', 'Content-Type');
 
     next();
 }
+
 app.use(allowCrossDomain);
-app.get('/', function(req, res){
-    //if(<parametros que você deseja testar>)
-    res.send('oi');
-    //else
-    //quando nao retornar os parametros que vc deseja testar
+
+const mocksDir = './mocks/';
+
+app.get('/generate', function (req, res) {
+    var mockBase = "app = require('express')();var allowCrossDomain = function(req, res, next) {\n\
+        res.header('Access-Control-Allow-Origin', '*');\n\
+        res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');\n\
+        res.header('Access-Control-Allow-Headers', 'Content-Type');\n\
+        next();\n\
+    }; app.use(allowCrossDomain);"
+
+    var configs;
+    fs.writeFile('./mockserver/mock.js', '', (err) => {
+        if (err) throw err;
+        console.log('The server was created!');
+    });
+    
+    fs.readdir(mocksDir, (err, files) => {
+        var i = 1;
+        var loadeds="";
+        files.forEach(file => {
+            fs.readFile(mocksDir + '/' + file, 'utf8', function (err, content) {
+                configs = JSON.parse(content);
+
+                var mockServer = "app." + configs.method + "('/" + file.substring(0, file.length - 4) + "', \n\
+                function(req, res){\n\
+                    if(Math.floor((Math.random() * 100) + 1) >"+configs.failChance+"){\n\
+                        res.json("+ configs.success + ")\n\
+                    }else{\n\
+                        res.json("+ configs.success + ")\n\
+                    }\n\
+                });"
+                
+            console.log('Creating ', file.substring(0, file.length - 4), '...');
+            loadeds+='"'+file.substring(0, file.length - 4)+'"'
+   
+                fs.appendFile('./mockserver/mock.js', mockServer, function (err, content) {
+                    console.log(i++, 'mocks loadeds of ', files.length );
+                    if(i == files.length){
+                        require('./mockServer/mock.js');
+                        console.log('MOCKSERVER UP!');
+                        res.json({'created':loadeds});
+                    }
+                });
+            });
+        });
+    })
 });
 
 
-app.listen(80, function(){
+app.listen(80, function () {
     console.log('server on');
 });
