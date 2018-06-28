383902ddda325c60a3e9167b8164e03f66df9085
p.beani <pedro.beani@madeinweb.com.br>
2018-06-28 15:40:33 -0300

    Adicionando content-type no header

--- easymock.js
+++ easymock.js
@@ -16,7 +16,8 @@ var allowCrossDomain = function (req, res, next) {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
     res.header('Access-Control-Allow-Headers', 'Content-Type');
-
+    res.header('Content-Type', 'Application/JSON');
+    
     next();
 }
 
